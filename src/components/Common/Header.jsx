5a37e85fd9e4b6b71e97c1efcf785b076b99fbea
i-manaswiniee <143373040+i-manaswiniee@users.noreply.github.com>
2024-03-04 20:49:00 +0530

    Create Header.jsx

---
+++ src/components/Common/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <div className="home-header">
+      <a href="#">
+        <img src="./Group 46.png" alt="contact" />
+      </a>
+      <div className="notify">
+        <img src="moptro logo.png" alt="moptro" />
+        <div>
+          <p>4</p>
+        </div>
+      </div>
+    </div>
+  );
+}
